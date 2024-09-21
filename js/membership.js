f3d9ce994fdb419dbad62c7a617ef106f961d71c
Andrew Kim <107439239+i30101@users.noreply.github.com>
2024-09-20 20:53:25 -0400

    fixed

--- js/membership.js
+++ js/membership.js
@@ -1,5 +1,6 @@
-setTimeout(() => {
-    function openApplicationForm() {
-        document.open("https://forms.gle/Y41dBiAZgpLcd3Kw6", "", "noopener=true")
-    }    
-}, 1000);
+function openApplicationForm() {
+    setTimeout(() => {
+        document.open("https://forms.gle/Y41dBiAZgpLcd3Kw6", "", "noopener=true");
+        console.log("success");
+    }, 1000);
+}
